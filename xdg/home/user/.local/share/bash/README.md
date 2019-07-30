813c3d700e2a8ca9169610a4518b10b4e971b5a0
Craig Sloggett <csloggett@evertz.com>
2019-07-30 10:24:07 -0400

    updates

--- xdg/home/user/.local/share/bash/README.md
+++
@@ -1,1 +0,0 @@
-A placeholder to keep the folder structure in git.
---
+++ xdg/populate_directories.sh
@@ -0,0 +1,20 @@
+#
+# populate_xdg_user_directories.sh
+#
+
+echo 'Generating user directories based on the XDG specification.'
+
+mkdir ~/.cache
+mkdir ~/.cache/vim
+mkdir ~/.cache/vim/backup
+mkdir ~/.cache/vim/swap
+mkdir ~/.cache/vim/undo
+mkdir ~/.cache/zsh
+mkdir ~/.local
+mkdir ~/.local/share
+mkdir ~/.local/share/bash
+mkdir ~/.local/share/gnupg
+mkdir ~/.local/share/less
+mkdir ~/.local/share/pass
+mkdir ~/.local/share/zsh
+mkdir ~/.config
