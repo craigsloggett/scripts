5086cd1a6a430ac62ff6b0bb7f9f6d63ef20f9ed
Craig Sloggett <craigsloggett@gmail.com>
2020-03-06 13:05:51 -0500

    updates

--- voidlinux/build_kernel.sh
+++ voidlinux/build_kernel.sh
@@ -3,17 +3,17 @@
 BOOTDIR=/mnt/boot
 TARGET=$BOOTDIR/EFI/boot
 CMDLINE_DIR=$BOOTDIR/
-EFISTUB=$BOOTDIR/linuxx64.efi.stub
+EFISTUB=$BOOTDIR/EFI/Linux/linuxx64.efi.stub
 
 for k in $BOOTDIR/vmlinuz*; do
 	NAME=$(basename $k | sed 's/vmlinuz-//')
-	INITRD="$BOOTDIR/initramfs-$NAME.img"
+	INITRDFILE="$BOOTDIR/initramfs-$NAME.img"
 	CMDLINE="$CMDLINE_DIR/cmdline.txt"
 
     echo "root=/dev/nvme0n1p2 rw initrd=\\initramfs-$NAME.img" > "$CMDLINE"
 
 	objcopy \
-	    --add-section .osrel="$BOOTDIR/os-release" --change-section-vma .osrel=0x20000 \
+	    --add-section .osrel="/mnt/etc/os-release" --change-section-vma .osrel=0x20000 \
 	    --add-section .cmdline="$CMDLINE" --change-section-vma .cmdline=0x30000 \
 	    --add-section .linux="$k" --change-section-vma .linux=0x40000 \
 	    --add-section .initrd="$INITRDFILE" --change-section-vma .initrd=0x3000000 \
