b907e5b7d26320c1602356be64ad0970b0f7d66d
Craig Sloggett <craig.sloggett@slalom.com>
2020-06-18 09:38:51 -0400

    Updates

--- macOS/bootstrap.sh
+++ macOS/bootstrap.sh
@@ -3,7 +3,7 @@
 # bootstrap.sh - macOS installation shell script.
 
 main() {
-    # homebrew
+    # homebrew (taken from their website)
     #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
     # system configuration
@@ -14,79 +14,8 @@ main() {
 
     cd "${HOME}"/Source/GitHub/craigsloggett/scripts/macOS
 
-    "$(./macsh)"
+    source macsh
 }
 
 main "$@"
 
-#
-#    # Clone dotfiles.
-#    mkdir -p "${SOURCE_DIR}"/GitHub/craigsloggett
-#    cd "${SOURCE_DIR}"/GitHub/craigsloggett
-#    
-#    git clone -b macOS https://github.com/craigsloggett/dotfiles.git
-#
-#    # Configure zsh
-#    cd dotfiles/zsh
-# Setup ZSH
-#sudo install -C -m 644 etc/zshenv /etc/zshenv
-
-## Setup XDG Directories
-#mkdir -p "${_XDG_CONFIG_HOME}"
-#mkdir -p "${_XDG_CACHE_HOME}"
-#mkdir -p "${_XDG_DATA_HOME}"
-
-## SSH
-#mkdir -p "${HOME}"/.ssh
-#scp "${SECURITY_USER}"@"${SECURITY_HOST}":"${SECURITY_DIR}"/ssh/"${COMPANY_NAME}"/id_ed25519 "${HOME}"/.ssh
-#scp "${SECURITY_USER}"@"${SECURITY_HOST}":"${SECURITY_DIR}"/ssh/"${COMPANY_NAME}"/id_ed25519.pub "${HOME}"/.ssh
-
-# Export the values in /etc/zshenv
-
-# Setup GPG
-
-# Setup Pash
-
-# Setup Firefox
-
-#637387
-
-#!/usr/bin/env sh
-
-# Default values for blank parameters
-#DEBUG=0
-#IN_FILE=/etc/some-input-file.conf
-#OUT_FILE=/var/log/some-output-file.log
-#
-## Option parser, the order doesn't matter
-#while [ $# -gt 0 ]; do
-#    case "$1" in
-#        -i|--input)
-#            IN_FILE="$2"
-#            shift 2
-#            ;;
-#        -o|--output)
-#            OUT_FILE="$2"
-#            shift 2
-#            ;;
-#        --debug) # Argument acting as a simple flag
-#            DEBUG=1
-#            shift 1
-#            ;;
-#        *)
-#            break
-#            ;;
-#    esac
-#done
-#
-## Some simple argument checks
-#wrong_arg() {
-#    echo "Error: invalid value for $1" >&2
-#    exit 2
-#}
-#
-#[ -f $IN_FILE ] || wrong_arg "input file"
-#[ -f $OUT_FILE ] || wrong_arg "output file"
-#
-## The actual script can start below
-## ...
