35654b209acb278382c5303f5673b23652e098aa
Craig Sloggett <craig.sloggett@slalom.com>
2020-06-19 09:32:28 -0400

    Updates

--- macOS/bootstrap.sh
+++ macOS/bootstrap.sh
@@ -10,7 +10,10 @@ main() {
     mkdir -p "${HOME}"/Source/GitHub/craigsloggett
 
     cd "${HOME}"/Source/GitHub/craigsloggett
-    rm -rf scripts
+
+    if [[ -d "${HOME}"/Source/GitHub/craigsloggett/scripts ]]; then
+        rm -rf "${HOME}"/Source/GitHub/craigsloggett/scripts
+    fi
     git clone https://github.com/craigsloggett/scripts.git
 
     cd "${HOME}"/Source/GitHub/craigsloggett/scripts/macOS
