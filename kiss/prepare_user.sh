1b6400be1d9497be12a278cece6b092541127f7c
Craig Sloggett <craigsloggett@gmail.com>
2020-12-01 23:44:35 +0000

    add user

---
+++ kiss/prepare_user.sh
@@ -0,0 +1,26 @@
+#!/bin/sh -e
+
+# Setup XDG
+
+readonly TARGET_FILE="/etc/profile.d/xdg-base-directory-specification.sh"
+
+touch "${TARGET_FILE}"
+chmod 644 "${TARGET_FILE}"
+chown root:root "${TARGET_FILE}"
+
+cat <<- 'EOF' > "${TARGET_FILE}"
+# XDG Base Directory Specification environment variables.
+
+# User	
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+
+# System
+export XDG_DATA_DIRS="/usr/local/share:/usr/share"
+export XDG_CONFIG_DIRS="/etc/xdg"
+EOF
+
+# Create a regular user
+
+adduser nerditup
