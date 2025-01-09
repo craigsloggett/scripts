25e7d1d765bb42603ea91bb50e94f905298aafea
Craig Sloggett <craigsloggett@gmail.com>
2025-01-09 13:58:41 -0500

    Update install.sh script

--- macOS/get-defaults-domains.sh
+++ macOS/get-defaults-domains.sh
@@ -1,43 +1,85 @@
 #!/bin/sh
 
-output_filename="defaults-by-domain.txt"
+# Available configuration that can be set using the `defaults` command:
 
-# Clear the list of defaults available.
-: >"${output_filename}"
+# User preferences:
+#
+# defaults read -g # (this is equal to `defaults read .GlobalPreferences`)
+# defaults read <domain>
+# defaults read <path_to_plist_file>
+#
+# System preferences:
+#
+# sudo defaults read -g # (this is equivalent to `sudo defaults read .GlobalPreferences`)
+# sudo defaults read <domain>
+# sudo defaults read <path_to_plist_file>
+
+# Currently known locations of plist files:
+#
+# ~/Library/Preferences/
+# ~/Library/Containers/
+# /Library/Preferences/
+# /Library/Managed Preferences/
+
+defaults_by_domain_filename="defaults-by-domain.txt"
+defaults_by_path_filename="defaults-by-path.txt"
+
+# Clear the list of available defaults.
+: >"${defaults_by_domain_filename}"
+: >"${defaults_by_path_filename}"
 
 # Get a list of domains available by default to the `defaults` command.
-domains="$(defaults domains | tr ',' '\n' | tr -d ' ' | sort -u)"
+domains="$(defaults domains | tr ',' '\n' | sed 's/^ *//' | sort -u)"
 
 printf '%s\n' "${domains}" |
   while read -r domain; do
+
     # Even though this domain appears in `defaults domains`, it may be
     # restricted, ephemeral, or otherwise unreadable. We run `defaults read`
     # to confirm it actually contains readable preferences. If this fails,
     # we skip it.
     if defaults read "${domain}" >/dev/null 2>&1; then
-      printf '%s\n' "${domain}" >>"${output_filename}"
+      printf '%s\n' "${domain}" >>"${defaults_by_domain_filename}"
     fi
   done
 
 # There are domains available to a regular user that can be specified by domain
 # (not the full path) that are not listed using the `defaults domains` command.
-find ~/Library/Preferences -type f -name "*.plist" |
-  while read -r plist; do
-    # Get the domain from the full path in the same format as those available
-    # by the `defaults` command.
-    domain="$(basename "${plist}" '.plist')"
+for directory in "${HOME}/Library/Preferences" "${HOME}/Library/Containers"; do
 
-    # We have already gone over the domains listed by the `defaults domains`
-    # command so we take only domains that are not listed.
-    if ! printf '%s\n' "${domains}" | grep -qx "${domain}"; then
-      # Even though this domain has an associated plist file, it may be
-      # restricted, ephemeral, or otherwise unreadable. We run `defaults read`
-      # to confirm it actually contains readable preferences. If this fails,
-      # we skip it.
-      if defaults read "${domain}" >/dev/null 2>&1; then
-        printf '%s\n' "${domain}" >>"${output_filename}"
-      fi
-    fi
-  done
+  # Some of the directories have restricted access so we run `find` against the
+  # directory and if it fails, we skip it.
+  if find "${directory}" >/dev/null 2>&1; then
 
-sort -u "${output_filename}" -o "${output_filename}"
+    find "${directory}" -type f -name "*.plist" |
+      while read -r plist; do
+
+        # Get the domain from the full path in the same format as those available
+        # by the `defaults` command.
+        domain="$(basename "${plist}" '.plist')"
+
+        # We have already gone over the domains listed by the `defaults domains`
+        # command so we take only domains that are not listed.
+        if ! printf '%s\n' "${domains}" | grep -qx "${domain}"; then
+
+          # Even though this domain has an associated plist file, it may be
+          # restricted, ephemeral, or otherwise unreadable. We run `defaults read`
+          # to confirm it actually contains readable preferences. If this fails,
+          # we skip it.
+          if defaults read "${domain}" >/dev/null 2>&1; then
+            printf '%s\n' "${domain}" >>"${defaults_by_domain_filename}"
+          fi
+
+          # And the same but for the domains that must be specified using the
+          # full path to the plist file.
+          if defaults read "${plist}" >/dev/null 2>&1; then
+            printf '%s\n' "${plist}" >>"${defaults_by_path_filename}"
+          fi
+        fi
+
+      done
+  fi
+done
+
+sort -u "${defaults_by_domain_filename}" -o "${defaults_by_domain_filename}"
+sort -u "${defaults_by_path_filename}" -o "${defaults_by_path_filename}"
