c50d282bef891d3cd21ababcbf0c8b5d78ae5781
Craig Sloggett <craigsloggett@gmail.com>
2025-01-09 11:13:26 -0500

    Update install.sh script

--- macOS/get-defaults-domains.sh
+++ macOS/get-defaults-domains.sh
@@ -1,37 +1,42 @@
 #!/bin/sh
 
 output_filename="defaults-by-domain.txt"
-domains="$(defaults domains | tr ',' '\n' | tr -d ' ' | sort -u)"
 
+# Clear the list of defaults available.
 : >"${output_filename}"
 
-defaults domains | tr ',' '\n' | tr -d ' ' | sort -u |
+# Get a list of domains available by default to the `defaults` command.
+domains="$(defaults domains | tr ',' '\n' | tr -d ' ' | sort -u)"
+
+printf '%s\n' "${domains}" |
   while read -r domain; do
-    printf '\n%s\n' "Attempting to read the domain it using the defaults command..."
-    printf '%s\n' "defaults read ${domain}"
-    if ! defaults read "${domain}" >/dev/null 2>&1; then
-      printf '%s\n' "The defaults command failed to read the domain."
-    else
-      printf '%s\n' "Success! Adding the domain to a list for future use."
+    # Even though this domain appears in `defaults domains`, it may be
+    # restricted, ephemeral, or otherwise unreadable. We run `defaults read`
+    # to confirm it actually contains readable preferences. If this fails,
+    # we skip it.
+    if defaults read "${domain}" >/dev/null 2>&1; then
       printf '%s\n' "${domain}" >>"${output_filename}"
     fi
   done
 
+# There are domains available to a regular user that can be specified by domain
+# (not the full path) that are not listed using the `defaults domains` command.
 find ~/Library/Preferences -type f -name "*.plist" |
   while read -r plist; do
-    printf '\n%s\n' "Searching for ${plist} in default domains..."
+    # Get the domain from the full path in the same format as those available
+    # by the `defaults` command.
     domain="$(basename "${plist}" '.plist')"
-    if printf '%s\n' "${domains}" | grep -qx "${domain}"; then
-      printf '%s\n' "The plist was found in the default domains list, attempting to read it using the defaults command..."
-      printf '%s\n' "defaults read ${domain}"
-      if ! defaults read "${domain}" >/dev/null 2>&1; then
-        printf '%s\n' "The defaults command failed to read the domain."
-      else
-        printf '%s\n' "Success! Adding the domain to a list for future use."
+
+    # We have already gone over the domains listed by the `defaults domains`
+    # command so we take only domains that are not listed.
+    if ! printf '%s\n' "${domains}" | grep -qx "${domain}"; then
+      # Even though this domain has an associated plist file, it may be
+      # restricted, ephemeral, or otherwise unreadable. We run `defaults read`
+      # to confirm it actually contains readable preferences. If this fails,
+      # we skip it.
+      if defaults read "${domain}" >/dev/null 2>&1; then
         printf '%s\n' "${domain}" >>"${output_filename}"
       fi
-    else
-      printf '%s\n' "N"
     fi
   done
 
